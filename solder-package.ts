87f9c463dc221fcd6ca9400a180fd2b7220445e1
mcd1992 <theanonbutinf@gmail.com>
2018-11-22 20:07:55 -0600

    Finally a working version getter

--- solder-package.ts
+++ solder-package.ts
@@ -17,8 +17,10 @@ var fs = require('fs');
 var util = require('util');
 var path = require('path');
 var mkdirp = require('mkdirp');
+var printf = require('printf');
 var spawnSync = require('child_process').spawnSync;
 var JSZip = require("jszip");
+const dJSON = require('dirty-json');
 var ArgumentParser = require('argparse').ArgumentParser;
 var parser = new ArgumentParser({ addHelp: true });
 parser.addArgument(
@@ -46,13 +48,20 @@ function getMCMOD(jar_path) {
     try {
       // var escaped_json = unzip.stdout.toString().replace('\n', '\\n');
       // Some mods think its okay to have raw newlines in their strings...
-      var mcmod_json = JSON.parse(unzip.stdout)[0];
+      var mcmod_json = dJSON.parse(unzip.stdout);
     } catch(e) {
       return {};
     };
-    return mcmod_json;
+
+    if (Array.isArray(mcmod_json)) {
+      return mcmod_json[0];
+    } else if (mcmod_json.modList) {
+      return mcmod_json.modList[0];
+    } else {
+      return mcmod_json;
+    }
   } else {
-    console.error(jar_path + " has no mcmod.info!");
+    console.log(jar_path + " has no mcmod.info!");
   }
   return {};
 }
@@ -65,7 +74,8 @@ export interface IModInfo {
   zip_name: string,
   zip_path: string,
   manifest: object,
-  mcmod: object
+  mcmod: object,
+  debug: string
 };
 
 var to_package: IModInfo[] = []; // Will contain the mods that need to be packaged
@@ -75,48 +85,39 @@ for (var section in manifest) {
     var mod_slug = mod_id.split(':')[1];
     var mod_mani = manifest[section][mod_id];
     var jar_path = util.format("%s/%s/%s", args.mods_dir, section, mod_mani.filename);
+    var mod_debug = ''; // TODO: Remove
 
     // Holy shit getting a clean mod version is such a pain...
     var mod_ver: string | undefined = undefined;
     var mcmod_json = getMCMOD(jar_path);
-    if (mcmod_json && (mcmod_json.mcversion || mcmod_json.version)) {
-      //if (!mcmod_json.mcversion.includes('$') && !mcmod_json.version.includes('$')
+    if (mcmod_json && (mcmod_json.version || mcmod_json.mcversion)) {
+      mod_debug += ' mcmod good ';
       var strip_re = /[^A-Za-z0-9\.\-]*/g; // Strip out all non-version-like
       var version_re = /.*?\-?([A-Za-z0-9\.\-]+[0-9A-Za-z])/; // Match only version-like without trail/head `-.`
-      if (mcmod_json.version.includes(mcmod_json.mcversion)) { // Sometimes devs put the fucking mc ver inside the regular ver...
-        var clean_ver = mcmod_json.version.replace(strip_re, '');
-        var check_re = version_re.exec(clean_ver);
-        mod_ver = (check_re && check_re[1]) ? check_re[1] : undefined;
+      var clean_ver = mcmod_json.version ? mcmod_json.version.replace(strip_re, '') : undefined;
+      var clean_mcver = mcmod_json.mcversion ? mcmod_json.mcversion.replace(strip_re, '') : undefined;
+      if ((mcmod_json.version && mcmod_json.version.includes('$')) || (mcmod_json.mcversion && mcmod_json.mcversion.includes('$'))) {
+        // Developer forgot to populate their ${version} variables in the mcmod.info...
+        // Just get the version from the filename
+      } else if (clean_ver && clean_ver.includes(clean_mcver)) { // Sometimes devs put the mc ver inside the regular ver
+        mod_debug += ' ver has mcver ';
+        mod_ver = clean_ver;
       } else {
-        // Holy fuck sometimes devs dont fill out both mcversion/version and sometimes they forget to run their package scripts and leave ${mcversion} in...
-        var clean_ver = mcmod_json.version ? (mcmod_json.version.includes('$') ? undefined : mcmod_json.version.replace(strip_re, '')) : undefined;
-        var clean_mcver = mcmod_json.mcversion ? (mcmod_json.mcversion.includes('$') ? undefined : mcmod_json.mcversion.replace(strip_re, '')) : undefined;
-        var concat = '';
-        if (clean_ver) {
-          concat += clean_ver
-        }
-        if (clean_mcver) {
-          concat += '-' + clean_mcver;
-        }
-        var check_re = version_re.exec(concat);
-        mod_ver = (check_re && check_re[1]) ? check_re[1] : undefined;
+        mod_debug += ' build mcver + ver ';
+
+        mod_ver = (clean_ver ? clean_ver : '') + (clean_mcver ? '-' + clean_mcver : '');
       }
     }
-    if (mod_ver === undefined) {
-      //console.error(mod_slug, " missing/improper mcmod.info");
-      // Fallback to version found in filename
+    if (mod_ver === undefined) { // Fallback to version found in filename
+      console.error(mod_slug, " missing/improper mcmod.info");
       var filename = path.basename(jar_path);
-      var version_re = /.+?([0-9\.\-]+)\.jar/;
+      var version_re = /.+?([0-9\.\-]+).*?\.jar/;
       var version = version_re.exec(filename);
       if (version && version[1]) {
-        mod_ver = version[1];
+        mod_ver = version[1].replace(/^[\-\.]*|[\-\.]*$/g, ''); // trim - and .
       } else {
         mod_ver = 'undefinedbecausetrashmod';
       }
-      /* TODO:
-        undefinedbecausetrashmod 	 treecapitator-port-undefinedbecausetrashmod.zip
-        undefinedbecausetrashmod 	 projecte-undefinedbecausetrashmod.zip
-      */
     }
 
     var zip_name = util.format("%s-%s.zip", mod_slug, mod_ver);
@@ -129,7 +130,8 @@ for (var section in manifest) {
       zip_name: zip_name,
       zip_path: zip_path,
       manifest: mod_mani,
-      mcmod: mcmod_json
+      mcmod: mcmod_json,
+      debug: mod_debug
     };
     to_package.push(todo);
   }
@@ -157,9 +159,9 @@ function package_mod(index) {
 // Create SQL file for importing into DB
 for (var key in to_package) {
   var mod = to_package[key];
-  if (mod.zip_name.includes('--') || mod.zip_name.includes('..')) {
-    console.log('\n', mod);
-  }
-  console.log(mod.mod_ver, '\t', mod.zip_name);
+  console.log(printf("%-40.40s %s", mod.mod_ver ? mod.mod_ver : '', mod.zip_name ? mod.zip_name : ''));
+  //if (mod.zip_name && (mod.zip_name.includes('--') || mod.zip_name.includes('..') || mod.zip_name.includes('undefinedbecause'))) {
+  //  console.error('\n', mod);
+  //}
   //console.log('\n', JSON.stringify(mod, null, 4));
 }
